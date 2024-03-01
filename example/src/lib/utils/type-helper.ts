a09e2a789bdb9a18c5baffe952e0a21463e01b2a
hoonjoo-park <hoonjoo94@naver.com>
2024-03-01 18:15:55 +0900

    feat: add type-helper functions(isNill,isNull, isEmpty)

---
+++ example/src/lib/utils/type-helper.ts
@@ -0,0 +1,11 @@
+export const isNill = (value: any): value is null | undefined => {
+  return value === null || value === undefined
+}
+
+export const isNull = (value: any): value is null => {
+  return value === null
+}
+
+export const isEmpty = (value: any): value is '' => {
+  return value === ''
+}
